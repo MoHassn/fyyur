4a774f8f20d1d10dc10818f6d27baa4ea7f904d0
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2021-07-21 21:13:27 +0200

    move models to a separate file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -15,6 +15,7 @@ from forms import *
 from past import *
 from flask_migrate import Migrate
 import sys
+from models import *
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -22,84 +23,8 @@ import sys
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
-
-
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-# I intended to make genres many to many relationship
-# but I found it will result in chages to the form and the front-end
-# so I will come back later
-# The many to many relationship to enable venue to have multiple genres
-# venue_genres = db.Table('venue_genres',
-#   db.Column('venue_id',db.Integer, db.ForeignKey('Venue.id', primary_key=True)),
-#   db.Column('genres_id',db.Integer, db.ForeignKey('Genres.id', primary_key=True))
-# )
-
-# the shows table
-# shows = db.Table('Shows',
-#   db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id', primary_key=True)),
-#   db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id', primary_key=True)),
-#   db.Column('start_time', db.DateTime, nullable=False)
-# )
-class Shows(db.Model):
-  __tablename__ = 'Shows'
-  __table_args__ = (
-    db.PrimaryKeyConstraint('venue_id', 'artist_id'),
-  )
-  venue_id = db.Column( db.Integer, db.ForeignKey('Venue.id', primary_key=True))
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id', primary_key=True))
-  start_time = db.Column(db.String(120))
-  venue = db.relationship("Venue", back_populates='shows')
-  artist = db.relationship("Artist", back_populates='shows')
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String(500))
-    genres = db.Column(db.String(120))
-    shows = db.relationship('Shows', back_populates='venue')
-
-    # shows = db.relationship('Shows', secondary=shows, backref=db.backref('venues', lazy=True))
-    # genres = db.relationship('Genres',secondary=venue_genres, backref=db.backref('venues', lazy=True) )
-
-
-# class Genres(db.Model):
-#   __tablename__ = 'Genres'
-
-#   id = db.Column(db.Integer, primary_key=True)
-#   name = db.Column(db.String(), nullable=False)
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String(500))
-    shows = db.relationship('Shows', back_populates='artist')
 
 
 
